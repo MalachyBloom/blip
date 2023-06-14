7208e6615f4f016be7c13c0fa59d8c59d1c7f8ae
MalachyBloom <bloomm@carleton.edu>
2023-06-14 12:54:52 -0500

    Update angular_localization.py

--- blip/tools/angular_localization.py
+++ blip/tools/angular_localization.py
@@ -250,4 +250,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     
-    getLocalizationSummary(args.rundir,outdir=args.outfir)
+    getLocalizationSummary(args.rundir,outdir=args.outdir)
